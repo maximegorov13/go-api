c9435df4a387f137971b63a5c5cda6012053d914
Maxim Egorov <maximegorov1998@yandex.ru>
2025-02-05 14:47:10 +0500

    Подключение GORM

---
+++ migrations/auto.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/joho/godotenv"
+	"github.com/maximegorov13/go-api/internal/link"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"os"
+)
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		panic(err)
+	}
+	db, err := gorm.Open(postgres.Open(os.Getenv("DSN")), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	db.AutoMigrate(&link.Link{})
+}
